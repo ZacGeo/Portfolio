255474d3ca6decfd0b9f884f41eaee61ec52dac6
ZacGeo <129080359+ZacGeo@users.noreply.github.com>
2024-07-17 10:48:50 +0300

    removing the border

--- my-portofolio/src/components/NavBar.jsx
+++ my-portofolio/src/components/NavBar.jsx
@@ -1,16 +1,16 @@
 import logo from "../assets/logo4.png";
-import {FaLinkedin, FaGithub, FaTwitter} from "react-icons/fa";
+import {FaLinkedin, FaGithub, FaDiscord} from "react-icons/fa";
 
 const Navbar = () => {
     return (
     <nav className=" mb-20 flex items-center justify-between py-6">
        <div className="flex flex-shrink-0 items-center ">
-          <img src={logo} alt="logo"  className="w-20 h-20 rounded-full "/>
+          <img src={logo} alt="logo"  className="w-20 h-20 rounded-full " />
        </div>
        <div className="m-8 flex items-center justify-center gap-4 text-3xl">
         <FaGithub />
         <FaLinkedin />
-        <FaTwitter />
+        <FaDiscord />
        </div>
     </nav>
     );
