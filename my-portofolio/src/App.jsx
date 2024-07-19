1ff10051cf34f5720642e107734852d5cc2b6cf1
ZacGeo <129080359+ZacGeo@users.noreply.github.com>
2024-07-20 00:12:58 +0300

    bugs fixed

--- my-portofolio/src/App.jsx
+++ my-portofolio/src/App.jsx
@@ -3,15 +3,13 @@ import Navbar from "./components/NavBar";
 
 const App = () => {
   return (
-    <div className="overflow-x-hidden text-neutral-300 antialiased slec selection:bg-cyan-300 selection:text-cyan-900">
-      <div className="flixed top-0 -z-0 h-full w-full"></div>
-      <div class="absolute inset-0 -z-10 h-full w-full items-center px-5 py-24 [background:radial-gradient(125%_125%_at_50%_10%,#000_40%,#63e_100%)]"></div>
-      <div className="container mx-auto px-8"><Navbar />
-      <Hero />
-      </div>
-      
+    <div className="overflow-x-hidden text-neutral-300 antialiased slec selection:bg-cyan-300 selection:text-cyan-900 h-screen w-full [background:radial-gradient(125%_125%_at_50%_10%,#000_40%,#63e_100%)]">
+        <div className="max-w-[1450px] m-auto">
+          <Navbar />
+          <Hero />
+        </div>
     </div>
   );
-}
+};
 
 export default App;
--- my-portofolio/src/components/NavBar.jsx
+++ my-portofolio/src/components/NavBar.jsx
@@ -1,21 +1,37 @@
 import logo from "../assets/logo4.png";
-import {FaLinkedin, FaGithub, FaDiscord} from "react-icons/fa";
+import { FaLinkedin, FaGithub, FaDiscord } from "react-icons/fa";
 
 const Navbar = () => {
-    return (
-    <nav className=" mb-20 flex items-center justify-between py-6">
-       <div className="flex flex-shrink-0 items-center ">
-          <img src={logo} alt="logo"  className="w-20 h-20 rounded-full " />
-       </div>
-       <div className="m-8 flex items-center justify-center gap-4 text-3xl">
-         <a href="https://github.com/ZacGeo" target="_blank" rel="noopener noreferrer"><FaGithub /></a>
-         <a href="https://www.linkedin.com/in/george-zacharopoulos-2a87402a0/" target="_blank" rel="noopener noreferrer"><FaLinkedin /></a>
-         <a href="https://discordapp.com/users/zacha27" target="_blank" rel="noopener noreferrer"><FaDiscord /></a>
-  
-       </div>
+  return (
+    <nav className="flex items-center justify-between px-4 mt-2">
+      <div className="flex flex-shrink-0 items-center ">
+        <img src={logo} alt="logo" className="w-20 h-20 rounded-full " />
+      </div>
+      <div className="m-8 flex items-center justify-center gap-4 text-3xl">
+        <a
+          href="https://github.com/ZacGeo"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <FaGithub />
+        </a>
+        <a
+          href="https://www.linkedin.com/in/george-zacharopoulos-2a87402a0/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <FaLinkedin />
+        </a>
+        <a
+          href="https://discordapp.com/users/zacha27"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <FaDiscord />
+        </a>
+      </div>
     </nav>
-    );
-}; 
-    
+  );
+};
 
 export default Navbar;
